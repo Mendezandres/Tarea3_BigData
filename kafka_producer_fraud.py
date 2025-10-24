7f4a0c0c9d675b5c036d2f32420f308d777390c5
Mendezandres <122476598+Mendezandres@users.noreply.github.com>
2025-10-23 20:12:14 -0500

    Implement Kafka producer for fraud data
    Configure Kafka producer and send dataset row by row.

--- kafka_producer_fraud.py
+++ kafka_producer_fraud.py
@@ -2,12 +2,18 @@ from kafka import KafkaProducer
 import pandas as pd
 import time, json
 
+# Configurar productor
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
+# Cargar dataset
 df = pd.read_csv("/home/vboxuser/creditcard.csv")
 
+# Enviar datos uno por uno
 for _, row in df.iterrows():
     data = {"Time": row["Time"], "Amount": row["Amount"], "Class": row["Class"]}
     producer.send("fraud_topic", value=data)
+    print(f"Enviado: {data}")   # muestra lo que se envía
     time.sleep(0.5)
+
+producer.flush()  # Asegura que todo se envíe antes de cerrar
